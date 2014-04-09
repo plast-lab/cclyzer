3071921a23ddb2db0a8af7b34c35fcfeb14044a4
kferles <kferles@gmail.com>
2014-04-10 01:11:35 +0300

    Deleting useless files

--- csvgen/src/DirInfo.cpp
+++
@@ -1,7 +0,0 @@
-// DirInfo.cpp
-
-#include "DirInfo.h"
-
-const char * DirInfo::factsDir = "facts/";
-const char * DirInfo::entitiesDir = "facts/entities/";
-const char * DirInfo::predicatesDir = "facts/predicates/";
