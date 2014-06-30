cb000e1e8bad301216155e6a0086a0a066ec00c1
George Balatsouras <gbalats@gmail.com>
2014-06-30 19:10:06 +0300

    Adding protobuf-generating routine to blox.mk

--- logic/blox.mk
+++ logic/blox.mk
@@ -7,3 +7,9 @@ endef
 define deploy-datalog-project
   bloxbatch -script $1
 endef
+
+define generate-protobuf-message
+  bloxdisassembler -p $1 > $(1:%.lbb=%.pb)
+  cut-protobuf-message.sh $(1:%.lbb=%.pb) > tmp.pb
+  mv tmp.pb $(1:%.lbb=%.pb)
+endef
