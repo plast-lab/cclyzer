99a89df85716cf19b02e92069037d90f05519914
George Balatsouras <gbalats@gmail.com>
2014-09-16 04:45:29 +0300

    Creating analysis class.

--- llvm-datalog/__main__.py
+++ llvm-datalog/__main__.py
@@ -1,8 +1,5 @@
 import argparse
-import os
-import shutil
-
-from copper import *
+import copper
 
 def main():
     # Create CLI parser
@@ -11,32 +8,11 @@ def main():
                         help='directory containing LLVM bitcode files to be analyzed')
     parser.add_argument('-o', '--output-dir', metavar='DIRECTORY', required = True,
                         help='output directory')
-    # Parse arguments
-    args = parser.parse_args()
 
-    # Create analysis
-    analysis = Analysis(args)
-
-    # Create empty CSV directory
-    csv_dir = os.path.join(args.output_dir, 'facts')
-
-    print "Cleaning up older facts ..."
-    if os.path.exists(csv_dir):
-        shutil.rmtree(csv_dir)
-
-    os.makedirs(csv_dir)
-
-    # Generate facts
-    print "Exporting facts ..."
-    fact_generation(input_dir = args.input_dir, output_dir = csv_dir)
-    print "Stored facts in %s" % csv_dir
-
-    # Create empty workspace
-    workspace = os.path.join(args.output_dir, 'db')
-
-    # Create database
-    print "Importing to database ... %s" % workspace
-    create_database(workspace, csv_dir)
+    args = parser.parse_args()       # parse arguments
+    analysis = copper.Analysis(args) # create analysis
+    analysis.generate_facts()        # generate CSV facts
+    analysis.create_database()       # create database
 
 if __name__ == '__main__':
     main()
--- llvm-datalog/copper/__init__.py
+++ llvm-datalog/copper/__init__.py
@@ -1,40 +1,84 @@
 import os
+import shutil
 import subprocess
 
 from blox.template import scripts
 from copper.resource import unpacked_binary, unpacked_project
-
+from functools import wraps
 
 class Project(object):
     pass
 
 class Analysis(object):
+
     def __init__(self, obj):
         self._input_dir = obj.input_dir
         self._output_dir = obj.output_dir
 
-def fact_generation(input_dir, output_dir):
-    with unpacked_binary('fact-generator') as executable:
-        return subprocess.call([executable, "-i", input_dir, "-o", output_dir])
+    def inside_output_subdir(subdir):
+        """Decorator that temporarily changes output directory."""
+        def wrapper(f):
+            @wraps(f)
+            def wrapped(self, *f_args, **f_kwargs):
+                old_dir = self._output_dir
+                try:
+                    # Change output directory
+                    self._output_dir = os.path.join(old_dir, subdir)
+                    # Call original method
+                    return f(self, *f_args, **f_kwargs)
+                finally:
+                    # Change back to original directory
+                    self._output_dir = old_dir
+            return wrapped
+        return wrapper
+
+    def clear(f):
+        """Decorator that erases output directory."""
+        @wraps(f)
+        def wrapped(self, *f_args, **f_kwargs):
+            print "Cleaning up older facts ..." ####
+            # Clear previous contents
+            if os.path.exists(self._output_dir):
+                shutil.rmtree(self._output_dir)
+            # Call original method
+            return f(self, *f_args, **f_kwargs)
+        return wrapped
+
+    @clear
+    @inside_output_subdir('facts')
+    def generate_facts(self):
+        indir, outdir = self._input_dir, self._output_dir
+        # Create empty directory
+        os.makedirs(outdir)
+        # Generate facts
+        print "Exporting facts ..." ###
+        with unpacked_binary('fact-generator') as executable:
+            ret = subprocess.call([executable, "-i", indir, "-o", outdir])
+        # Error checking
+        if ret != 0:
+            raise SystemError()
+        # Store path to this output directory
+        os.unlink('facts')
+        os.symlink(outdir, 'facts')
+        print "Stored facts in %s" % outdir
+        return self
 
 
-def create_database(workspace, csv_dir):
-    # Create script template
-    tpl = scripts.LOAD_SCHEMA
+    @inside_output_subdir('db')
+    def create_database(self):
+        # Create script template
+        tpl = scripts.LOAD_SCHEMA
 
-    # Unpack required projects
-    with unpacked_project('schema') as schema_project:
-        with unpacked_project('import') as import_project:
-            # Create LogicBlox script mappping
-            mapping = {'workspace' : workspace,
-                       'schema'    : schema_project,
-                       'import'    : import_project,
-            }
-            # Create LogicBlox script
-            with open('load-schema.lb', 'w') as script:
-                script.write(tpl.substitute(mapping))
+        # Unpack required projects
+        with unpacked_project('schema') as schema_project:
+            with unpacked_project('import') as import_project:
+                # Create LogicBlox script mappping
+                mapping = {'workspace' : self._output_dir,
+                           'schema'    : schema_project,
+                           'import'    : import_project,
+                }
+                # Create LogicBlox script
+                with open('load-schema.lb', 'w') as script:
+                    script.write(tpl.substitute(mapping))
 
-            # os.chdir(os.path.dirname(workspace))
-            os.unlink('facts')
-            os.symlink(csv_dir, 'facts')
-            return subprocess.call(["bloxbatch", "-script", 'load-schema.lb'])
+                return subprocess.call(["bloxbatch", "-script", 'load-schema.lb'])
