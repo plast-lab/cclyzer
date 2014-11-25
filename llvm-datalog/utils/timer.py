9c4b98594337bf80187017bca3f8849ed1b7c2b5
George Balatsouras <gbalats@gmail.com>
2014-11-25 20:22:54 +0200

    Adding timer class.

---
+++ llvm-datalog/utils/timer.py
@@ -0,0 +1,18 @@
+from functools import wraps
+from timeit import default_timer
+
+class Timer(object):
+    def __init__(self, callback):
+        """A timing context manager."""
+        self.callback = callback
+
+    def __enter__(self):
+        self.start = default_timer()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.end = default_timer()
+        self.callback(self.end - self.start)
+
+    def elapsed_time(self):
+        return default_timer() - start
