458a2ad85ef0370621022f14000a3ccab330c2b6
George Balatsouras <gbalats@gmail.com>
2014-12-05 18:21:34 +0200

    Make bloxcompiler invocation version-specific.

--- src/logic/blox.mk
+++ src/logic/blox.mk
@@ -1,13 +1,26 @@
+#------------------
 # LogicBlox Tools
+#------------------
 
-define compile-datalog-project
-  bloxcompiler compileProject -outDir $2 $1
-endef
+BLOXCOMPILER_VERSION := $(shell bloxcompiler -version)
 
+# Compile project command
+ifneq (,$(findstring 3.9,$(BLOXCOMPILER_VERSION)))
+  define compile-datalog-project
+    bloxcompiler -compileProject $1 -outDir $2
+  endef
+else
+  define compile-datalog-project
+    bloxcompiler compileProject -outDir $2 $1
+  endef
+endif
+
+# Execute datalog script command
 define deploy-datalog-project
   bloxbatch -script $1
 endef
 
+# Disassemble protobuf command
 define generate-protobuf-message
   $(info Generating protobuf message file for $1 ...)
   bloxdisassembler -p $1 > $2
