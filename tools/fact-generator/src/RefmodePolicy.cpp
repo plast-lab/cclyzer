e4cedb6474794ac2942a31c3af673d853f574c6e
George Balatsouras <gbalats@gmail.com>
2015-07-06 19:07:18 +0300

    Fix #1. Now even metadata are printed fast.
    This happened by porting part of the ASM Writer logic and creating an
    internal slot tracker for unnamed metadata values. Together with some
    previous commits, the fact-generation step now enjoys a x20 speedup!!

--- tools/fact-generator/src/RefmodePolicy.cpp
+++ tools/fact-generator/src/RefmodePolicy.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #include <boost/algorithm/string.hpp>
 #include <llvm/Support/raw_ostream.h>
+#include <llvm/IR/Metadata.h>
 #include "RefmodePolicy.hpp"
 #include "LLVMEnums.hpp"
 
@@ -33,12 +34,63 @@ class RefmodePolicy::Impl : LLVMEnumSerializer {
         contexts.pop_back();
     }
 
-    void enterModule(const llvm::Module *module) {
+    void enterModule(const llvm::Module *module)
+    {
+        using namespace llvm;
+
         Mod = module;
+        mdnNext = 0;
+
+        // Add metadata used by named metadata.
+        for (Module::const_named_metadata_iterator
+                 I = module->named_metadata_begin(),
+                 E = module->named_metadata_end(); I != E; ++I)
+        {
+            const NamedMDNode *NMD = I;
+
+            for (unsigned i = 0, e = NMD->getNumOperands(); i != e; ++i)
+                createMetadataSlot(NMD->getOperand(i));
+        }
+
+        SmallVector<std::pair<unsigned, MDNode*>, 4> MDForInst;
+
+        // Iterate over functions
+        for (Module::const_iterator
+                 fi = Mod->begin(), fi_end = Mod->end(); fi != fi_end; ++fi)
+        {
+            // Iterate over basic blocks
+            for (Function::const_iterator
+                     bbi = fi->begin(),
+                     bbi_end = fi->end(); bbi != bbi_end; ++bbi)
+            {
+                // Iterate over instructions
+                for (BasicBlock::const_iterator
+                         I = bbi->begin(), E = bbi->end(); I != E; ++I)
+                {
+                    // Intrinsics can directly use metadata.  We allow direct calls to any
+                    // llvm.foo function here, because the target may not be linked into the
+                    // optimizer.
+                    if (const CallInst *CI = dyn_cast<CallInst>(I)) {
+                        if (Function *F = CI->getCalledFunction())
+                            if (F->isIntrinsic())
+                                for (unsigned i = 0, e = I->getNumOperands(); i != e; ++i)
+                                    if (MDNode *N = dyn_cast_or_null<MDNode>(I->getOperand(i)))
+                                        createMetadataSlot(N);
+                    }
+
+                    // Process metadata attached with this instruction.
+                    I->getAllMetadata(MDForInst);
+                    for (unsigned i = 0, e = MDForInst.size(); i != e; ++i)
+                        createMetadataSlot(MDForInst[i].second);
+                    MDForInst.clear();
+                }
+            }
+        }
     }
 
     void exitModule() {
         Mod = nullptr;
+        mdnMap.clear();
     }
 
   protected:
@@ -46,6 +98,16 @@ class RefmodePolicy::Impl : LLVMEnumSerializer {
 
     // Compute variable numberings
     static void computeNumbering(const llvm::Function *, std::map<const llvm::Value*,unsigned> &);
+
+    // Add metadata node
+    void createMetadataSlot(const llvm::MDNode *N);
+
+    // Retrieve metadata slot
+    int getMetadataSlot(const llvm::MDNode *N) const {
+        // Find the MDNode in the module map
+        std::map<const llvm::MDNode*, unsigned>::const_iterator MI = mdnMap.find(N);
+        return MI == mdnMap.end() ? -1 : (int) MI->second;
+    }
 
   private:
 
@@ -66,7 +128,12 @@ class RefmodePolicy::Impl : LLVMEnumSerializer {
     // Tracking local contexts
     std::vector<RefContext> contexts;
 
+    // Current module
     const llvm::Module *Mod;
+
+    // Mapping numbers to metadata nodes, module-wise
+    std::map<const llvm::MDNode*, unsigned> mdnMap;
+    unsigned mdnNext;
 };
 
 
@@ -192,28 +259,51 @@ refmode_t RefmodePolicy::Impl::refmodeOf(const llvm::Value * Val) const
     string rv;
     raw_string_ostream rso(rv);
 
-    do {
-        if (Val->hasName()) {
-            rso << (isa<GlobalValue>(Val) ? '@' : '%')
-                << Val->getName();
-            break;
-        }
+    if (Val->hasName()) {
+        rso << (isa<GlobalValue>(Val) ? '@' : '%')
+            << Val->getName();
+        goto print;
+    }
 
-        if (isa<Constant>(Val)) {
-            Val->printAsOperand(rso, false);
-            break;
-        }
+    if (isa<Constant>(Val)) {
+        Val->printAsOperand(rso, false);
+        goto print;
+    }
 
-        if (Val->getType()->isVoidTy()) {
-            Val->printAsOperand(rso, false, Mod);
-            break;
-        }
+    if (Val->getType()->isVoidTy()) {
+        Val->printAsOperand(rso, false, Mod);
+        goto print;
+    }
 
-        if (Val->getType()->isMetadataTy()) {
-            Val->printAsOperand(rso, false, Mod);
-            break;
-        }
+    if (Val->getType()->isMetadataTy()) {
+        const MDNode *N = cast<MDNode>(Val);
 
+        if (N->isFunctionLocal()) {
+            rso << "!{";
+
+            for (unsigned mi = 0, me = N->getNumOperands(); mi != me; ++mi) {
+                const Value *V = N->getOperand(mi);
+                if (!V)
+                    rso << "null";
+                else {
+                    rso << refmodeOf(V->getType())
+                        << ' '
+                        << refmodeOf(V);
+                }
+                if (mi + 1 != me)
+                    rso << ", ";
+            }
+
+            rso << "}";
+        } else {
+            // unnamed metadata value
+            rso << '!' << getMetadataSlot(N);
+        }
+        goto print;
+    }
+
+    // Handle unnamed variables
+    {
         unsigned N = contexts.size();
 
         while (N > 0) {
@@ -226,14 +316,15 @@ refmode_t RefmodePolicy::Impl::refmodeOf(const llvm::Value * Val) const
                     computeNumbering(cast<llvm::Function>(ctxt.anchor), ctxt.numbering);
 
                 rso << '%' << ctxt.numbering[Val];
-                return rso.str();
+                goto print;
             }
         }
+    }
 
-        // Expensive
-        Val->printAsOperand(rso, false, Mod);
-    } while(0);
+    // Expensive
+    Val->printAsOperand(rso, false, Mod);
 
+print:
     // Trim external whitespace
     string ref = rso.str();
     trim(ref);
@@ -288,4 +379,27 @@ void RefmodePolicy::Impl::computeNumbering(
     }
 
     assert(!numbering.empty() && "asked for numbering but numbering was no-op");
+}
+
+
+void RefmodePolicy::Impl::createMetadataSlot(const MDNode *N)
+{
+    assert(N);
+
+    // Don't insert if N is a function-local metadata, these are always printed
+    // inline.
+    if (!N->isFunctionLocal()) {
+        std::map<const MDNode*, unsigned>::iterator I = mdnMap.find(N);
+
+        if (I != mdnMap.end())
+            return;
+
+        unsigned DestSlot = mdnNext++;
+        mdnMap[N] = DestSlot;
+    }
+
+    // Recursively add any MDNodes referenced by operands.
+    for (unsigned i = 0, e = N->getNumOperands(); i != e; ++i)
+        if (const MDNode *Op = dyn_cast_or_null<MDNode>(N->getOperand(i)))
+            createMetadataSlot(Op);
 }
