c1293e45072f852dd4c87a592be61d98298d360a
George Balatsouras <gbalats@gmail.com>
2015-07-21 20:09:41 +0300

    Uncomment example memory.c

--- examples/memory.c
+++ examples/memory.c
@@ -3,27 +3,31 @@
 
 int main(int argc, char *argv[])
 {
+    int xarr[8];
+    int yarr[argc];
     int x, y, *p, **q;
 
     p = &x;
     q = &p;
     *q = &y;
 
-    /* int *ptr; */
+    int *ptr;
 
-    /* ptr = malloc(sizeof(int)); */
+    ptr = malloc(sizeof(int));
 
-    /* x = 5; */
-    /* *ptr = x; */
+    x = 5;
+    *ptr = x;
 
-    /* int y = *ptr; */
+    y = *ptr;
 
-    /* if (y != x) */
-    /*     return EXIT_FAILURE; */
+    if (y != x)
+        return EXIT_FAILURE;
 
-    /* ptr = &x; */
-    /* printf("%d", *ptr); */
-    /* ptr = &argc; */
+    ptr = &x;
+    printf("%d", *ptr);
+    ptr = &argc;
+
+    int *ptr2 = yarr;
 
     return EXIT_SUCCESS;
 }
