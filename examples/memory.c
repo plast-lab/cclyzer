66a0382a66870b03d989060d7a77866b4e49bfa7
George Balatsouras <gbalats@gmail.com>
2015-09-10 19:35:08 +0300

    Extend memory example.

--- examples/memory.c
+++ examples/memory.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int *ep;
+int ep2;
+int **ep3;
+
 int main(int argc, char *argv[])
 {
     int xarr[8];
@@ -30,4 +34,70 @@ int main(int argc, char *argv[])
     int *ptr2 = yarr;
 
     return EXIT_SUCCESS;
+}
+
+void *f1() {            /* constant address */
+    int *ptr = (int *) 0xabcd;
+    return ptr;
+}
+
+void *f2() {            /* global pointer */
+    int *ptr = ep;
+    int *ptr2 = &ep2;
+    int val = *ep;
+    int val2 = ep2;
+    return ptr;
+}
+
+void *f3() {            /* malloc */
+    int *ptr = malloc(100 * sizeof(int));
+    return ptr;
+}
+
+void *f4(int *ptr) {
+    return ptr + 5;
+}
+
+void *f5() {
+    void *(*fp)(int*);
+
+    fp = &f4;
+    return fp;
+}
+
+void *f6() {
+    int **array = malloc(10 * sizeof(int *));
+
+    for (int i = 0; i < 10; i++)
+        array[i] = malloc(100 * sizeof(int));
+
+    return array;
+}
+
+void foo() {
+    void *p, *q, *r, *s, *t;
+
+    p = f1();
+
+    ep = malloc(sizeof(int));
+    ep = (int *) p;
+
+    ep2 = (int) &ep;
+    *ep3 = ep;
+
+    q = f2();
+    r = f3();
+    s = f4(q);                  /*  */
+    t = f5();
+
+    int **u = f6();
+
+    int *u0 = *u;               /*  */
+    int *u1 = u[1];             /*  */
+}
+
+void bar(int cond) {
+    int x, y, *ptr;
+
+    ptr = cond > 0 ? &x : &y;
 }
