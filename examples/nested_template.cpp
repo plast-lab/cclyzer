aeefc58fc8d604675ef391ecdba91f4fde323f8f
George Balatsouras <gbalats@gmail.com>
2016-10-07 16:31:37 +0300

    Add nested template example

---
+++ examples/nested_template.cpp
@@ -0,0 +1,22 @@
+template<typename First, typename Second>
+class Pair {
+  private:
+    First first;
+    Second second;
+
+  public:
+    template<typename Third>
+    class Inner {
+        Third third;
+    };
+};
+
+int main(int argc, char *argv[])
+{
+    Pair<int,double>::Inner<char> p;
+    Pair<int,double>::Inner<long> p2;
+    Pair<int,int>::Inner<char> p3;
+    Pair<int,int>::Inner<double> p4;
+    Pair<double,double>::Inner<char> p5;
+    return 0;
+}
