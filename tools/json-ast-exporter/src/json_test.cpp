dc223c8a2af675aefa9402e978f308ee4f4079ff
George Balatsouras <gbalats@gmail.com>
2017-01-31 19:21:36 +0200

    Add json library

---
+++ tools/json-ast-exporter/src/json_test.cpp
@@ -0,0 +1,31 @@
+#include <sstream>
+#include <jsoncons/json.hpp>
+#include <jsoncons/json_filter.hpp>
+
+using namespace jsoncons;
+
+
+int main()
+{
+    std::string s = R"({"first":1, "second":2,"fourth":3,"fifth":4})";
+
+    json_serializer serializer(std::cout);
+
+    // Filters can be chained
+    rename_name_filter filter2("fifth", "fourth", serializer);
+    rename_name_filter filter1("fourth", "third", filter2);
+
+    // A filter can be passed to any function that takes
+    // a json_input_handler ...
+    std::cout << "(1) ";
+    std::istringstream is(s);
+    json_reader reader(is, filter1);
+    reader.read();
+    std::cout << std::endl;
+
+    // or a json_output_handler
+    std::cout << "(2) ";
+    ojson j = ojson::parse(s);
+    j.dump(filter1);
+    std::cout << std::endl;
+}
