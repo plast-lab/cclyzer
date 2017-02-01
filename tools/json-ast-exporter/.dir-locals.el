9b65bc90752d6eadfdf509fcb0c1e904f3a36eec
George Balatsouras <gbalats@gmail.com>
2017-02-01 16:00:49 +0200

    Add dir-locals for EMACS flychecking

---
+++ tools/json-ast-exporter/.dir-locals.el
@@ -0,0 +1,14 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c++-mode
+  (flycheck-checker . c/c++-gcc)
+  (flycheck-gcc-language-standard . "c++11")
+  (eval . (setq include-paths (list "../../jsoncons/src"
+                                    (replace-regexp-in-string
+                                     "\n$" ""
+                                     (shell-command-to-string
+                                      "llvm-config --includedir")))
+                flycheck-gcc-include-path include-paths
+                company-clang-arguments (mapcar (lambda (p) (concat "-I" p))
+                                                include-paths)))))
